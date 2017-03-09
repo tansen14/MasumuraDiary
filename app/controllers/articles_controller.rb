d74e285194518700698b8c4605864a6af3b70ee8
tansen14 <s1B101402816@gmail.com>
2017-03-09 02:19:10 +0000

    発展課題も足したし、全ページにアクセスできるようにLINKをちょっぴり追加したよ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,40 @@
 class ArticlesController < ApplicationController
 	def new
-		@article = Article.new
+	end
+
+	def create
+		article = Article.new
+		article.title = params[:title]
+		article.body = params[:body]
+		article.author = params[:author]
+		article.save
+		redirect_to '/articles/new'
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		article = Article.find(params[:id])
+		article.title = params[:title]
+		article.body = params[:body]
+		article.author = params[:author]
+		article.save
+		redirect_to article_path(article.id)
+	end
+
+	def destroy
+		article = Article.find(params[:id])
+		article.destroy
+  		redirect_to articles_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 	get '/top' => 'home#top'
-	get '/new' => 'articles#new'
+	get '/articles/new' => 'articles#new'
+	post '/articles' => 'articles#create'
+	get '/articles' => 'articles#index'
+	get '/articles/:id' => 'articles#show', as:'article'
+	patch '/article/:id' => 'articles#update', as:'update_article'
+	get '/article/:id/edit' => 'articles#edit', as:'edit_article'
+	delete '/article/:id' => 'articles#destroy', as:'destroy_article'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
